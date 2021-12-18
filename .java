6b06956fc74e28f0d23afc473f0e698b7f80490e
kamali <93373518+kamalikumar123@users.noreply.github.com>
2021-12-18 15:08:33 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        String str1 = "Learn Java";
+        String str2 = "learn java";
+        String str3 = "Learn Kolin";
+        int result;
+        }
+        }
